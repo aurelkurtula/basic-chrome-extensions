d4e92837570da685b3592802513ca6572f634a6d
marginal <aurelsmarginalia@gmail.com>
2017-11-19 15:36:12 +0000

    init

---
+++ extentionOne_conCrastinator/myscripts.js
@@ -0,0 +1,10 @@
+document.body.innerHTML = `
+<!doctype html>
+<html>
+  <body>
+<div class="wrap"
+style="background-image: url('${chrome.runtime.getURL("danger.jpeg")}')">
+        <h1>Get back to work you little...</h1>
+</div>
+  </body>
+</html>`
